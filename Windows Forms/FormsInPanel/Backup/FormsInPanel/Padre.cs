6169e7b5f5f0d14384461f17c9ac218bf5f4e2c6
nicolocodev <nicoherreraf@hotmail.com>
2014-12-10 10:19:43 -0500

    Actualizado. No crear nuevas instancias de los forms hijos

--- Windows Forms/FormsInPanel/Backup/FormsInPanel/Padre.cs
+++ Windows Forms/FormsInPanel/Backup/FormsInPanel/Padre.cs
@@ -20,8 +20,7 @@ namespace WindowsFormsApplication1
         {
             if (this.panelContenedor.Controls.Count > 0)
                 this.panelContenedor.Controls.RemoveAt(0);
-            Hijo1 form = Application.OpenForms.OfType<Hijo1>().FirstOrDefault();
-            Hijo1 hijo1 = form ?? new Hijo1();
+            Hijo1 hijo1 = new Hijo1();
             hijo1.TopLevel = false;
             hijo1.FormBorderStyle = FormBorderStyle.None;
             hijo1.Dock = DockStyle.Fill;
@@ -31,27 +30,25 @@ namespace WindowsFormsApplication1
         }
 
         private void btMostrarHijo2_Click(object sender, EventArgs e)
-        {
-            var form = Application.OpenForms.OfType<Hijo2>().FirstOrDefault();
-            Hijo2 hijo = form ?? new Hijo2();
-            AddFormInPanel(hijo);
+        {            
+            AddFormInPanel(new Hijo2());
         }
 
         /// <summary>
         /// Agrega en un Panel un formulario
         /// </summary>
         /// <param name="formHijo">Instancia del formulario a agregar</param>
-        private void AddFormInPanel(Form formHijo)
+        private void AddFormInPanel(object formHijo)
         {
             if (this.panelContenedor.Controls.Count > 0)
                 this.panelContenedor.Controls.RemoveAt(0);
-           
-            formHijo.TopLevel = false;
-            formHijo.FormBorderStyle = FormBorderStyle.None;
-            formHijo.Dock = DockStyle.Fill;
-            this.panelContenedor.Controls.Add(formHijo);
-            this.panelContenedor.Tag = formHijo;
-            formHijo.Show();
+            Form fh = formHijo as Form;
+            fh.TopLevel = false;
+            fh.FormBorderStyle = FormBorderStyle.None;
+            fh.Dock = DockStyle.Fill;
+            this.panelContenedor.Controls.Add(fh);
+            this.panelContenedor.Tag = fh;
+            fh.Show();
         }
 
         
